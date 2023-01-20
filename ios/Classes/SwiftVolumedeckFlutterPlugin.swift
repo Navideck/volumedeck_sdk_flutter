09c9978e6260558da7da2da157867443c2d926ff
Foti Dim <fdimanidis@gmail.com>
2023-01-20 12:14:01 +0100

    Add iOS support

--- ios/Classes/SwiftVolumedeckFlutterPlugin.swift
+++ ios/Classes/SwiftVolumedeckFlutterPlugin.swift
@@ -1,14 +1,52 @@
 import Flutter
 import UIKit
+import Volumedeck
 
-public class SwiftVolumedeckFlutterPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "volumedeck_flutter", binaryMessenger: registrar.messenger())
-    let instance = SwiftVolumedeckFlutterPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+public class SwiftVolumedeckFlutterPlugin: NSObject, FlutterPlugin { //, FlutterStreamHandler {
+    var volumedeck: Volumedeck?
+    // private var volumedeckEventsSink: FlutterEventSink?
+    
+    private func initVolumedeck(){
+        // volumedeck = Volumedeck(runInBackground: true)
+    }
+    
+    private func disposeVolumedeck(){
+        volumedeck = nil
+    }
+    
+    // public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+    //     guard let args = arguments as? Dictionary<String, Any>, let name = args["name"] as? String else {
+    //           return nil
+    //     }
+    //     if name == "volumedeckEvent" {
+    //         volumedeckEventsSink = events
+    //         initVolumedeck()
+    //     }
+    //     return nil
+    // }
+    
+    // public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+    //     guard let args = arguments as? Dictionary<String, Any>, let name = args["name"] as? String else {
+    //           return nil
+    //     }
+    //     if name == "volumedeckEvent" {
+    //         volumedeckEventsSink = nil
+    //         disposeVolumedeck()
+    //     }
+    //     return nil
+    // }
+    
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        
+        let channel = FlutterMethodChannel(name: "volumedeck", binaryMessenger: registrar.messenger())
+        let instance = SwiftVolumedeckFlutterPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+                
+        let eventChannel = FlutterEventChannel(name: "com.navideck.volumedeck", binaryMessenger: registrar.messenger())
+//        eventChannel.setStreamHandler(instance)
+        
+        instance.volumedeck = Volumedeck(runInBackground: true)
+        instance.volumedeck?.isOn = true
+    }
 }
--- ios/volumedeck_flutter.podspec
+++ ios/volumedeck_flutter.podspec
@@ -15,9 +15,12 @@ A new Flutter plugin project.
   s.source           = { :path => '.' }
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
+  s.dependency 'Volumedeck'
   s.platform = :ios, '9.0'
 
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
+
+  # s.vendored_frameworks = 'VolumedeckiOS.xcframework' # Evenutally we need to add it back and remove "s.dependency 'Volumedeck'" when we switch to the compiled xcframework
 end
