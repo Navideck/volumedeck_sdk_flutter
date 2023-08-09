a90bc89eedf798e4aad2f4933f7ab8cbeefdc53d
Rohit Sangwan <rohitsangwan647@gmail.com>
2023-08-09 17:42:05 +0530

    Refactor plugin

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,9 +25,10 @@ class _MyAppState extends State<MyApp> {
   void initializeVolumedeck() async {
     await Volumedeck.initialize(
       runInBackground: true,
+      autoStart: false,
       showStopButtonInAndroidNotification: true,
       showSpeedAndVolumeChangesInAndroidNotification: true,
-      onLocationStatusChange: (bool status) {
+      locationServicesStatusChange: (bool status) {
         setState(() => isLocationOn = status);
       },
       onStart: () {
@@ -49,12 +50,6 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     initializeVolumedeck();
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    Volumedeck.dispose();
-    super.dispose();
   }
 
   @override
--- ios/Classes/SwiftVolumedeckFlutterPlugin.swift
+++ ios/Classes/SwiftVolumedeckFlutterPlugin.swift
@@ -1,66 +1,52 @@
 import Flutter
-import UIKit
 import VolumedeckiOS
 
 public class SwiftVolumedeckFlutterPlugin: NSObject, FlutterPlugin {
-    var volumedeck: Volumedeck?
-    private var messageConnector: FlutterBasicMessageChannel?
-    
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(name: "@com.navideck.volumedeck_flutter", binaryMessenger: registrar.messenger())
-        let instance = SwiftVolumedeckFlutterPlugin()
-        instance.messageConnector = FlutterBasicMessageChannel(name: "@com.navideck.volumedeck_flutter/message_connector", binaryMessenger: registrar.messenger())
-        registrar.addMethodCallDelegate(instance, channel: channel)
+        let handler = VolumedeckChannelHandler(volumedeckCallback: VolumedeckCallback(binaryMessenger: registrar.messenger()))
+        VolumedeckChannelSetup.setUp(binaryMessenger: registrar.messenger(), api: handler)
     }
-    
-    
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        switch call.method {
-        case "initialize":
-            let args = call.arguments as? Dictionary<String, Any>
-            let runInBackground: Bool = args?["runInBackground"] as? Bool ?? false
-            let activationKey: String? = args?["activationKey"] as? String
-            initializeVolumedeck(runInBackground: runInBackground,activationKey: activationKey)
-            result(nil)
-        case "start":
-            volumedeck?.start()
-            result(nil)
-        case "stop":
-            volumedeck?.stop()
-            sendMessage(type: "onStop")
-            result(nil)
-        default:
-            result(FlutterMethodNotImplemented)
-        }
+}
+
+private class VolumedeckChannelHandler: NSObject, VolumedeckChannel {
+    var volumedeck: Volumedeck?
+    var volumedeckCallback: VolumedeckCallback
+
+    init(volumedeckCallback: VolumedeckCallback) {
+        self.volumedeckCallback = volumedeckCallback
     }
-    
-    
-    public func initializeVolumedeck(runInBackground: Bool, activationKey: String?) {
+
+    func initialize(
+        autoStart: Bool,
+        runInBackground: Bool,
+        showStopButtonInAndroidNotification _: Bool,
+        showSpeedAndVolumeChangesInAndroidNotification _: Bool,
+        activationKey: String?
+    ) throws {
         volumedeck = Volumedeck(
             runInBackground: runInBackground,
             onLocationUpdate: { speed, volume in
-                self.sendMessage(
-                    type: "onLocationUpdate",
-                    data: [
-                        "speed": speed,
-                        "volume": volume,
-                    ]
-                )
+                self.volumedeckCallback.onLocationUpdate(speed: speed, volume: volume) {}
             },
             onLocationStatusChange: { status in
-                self.sendMessage(type: "onLocationStatusChange", data: status)
+                self.volumedeckCallback.onLocationStatusChange(status: status) {}
             },
             onStart: {
-                self.sendMessage(type: "onStart")
+                self.volumedeckCallback.onStart {}
             },
-            activationKey: activationKey
+            onStop: {
+                self.volumedeckCallback.onStop {}
+            },
+            activationKey: activationKey,
+            autoStart: autoStart
         )
     }
-    
-    public func sendMessage(type: String, data: Any? = nil) {
-        messageConnector?.sendMessage([
-            "type": type,
-            "data": data,
-        ])
+
+    func start() throws {
+        volumedeck?.start()
+    }
+
+    func stop() throws {
+        volumedeck?.stop()
     }
 }
