294674fb50f782a38f75ae9cab871fba2dddc451
Rohit Sangwan <rohitsangwan647@gmail.com>
2023-09-13 17:25:40 +0530

    Bump native libraries to 1.3.0 (#11)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: avoid_print
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:volumedeck_flutter/volumedeck_flutter.dart';
 
 void main() {
@@ -22,8 +26,28 @@ class _MyAppState extends State<MyApp> {
   double speed = 0.0;
   double volume = 0.0;
 
+  Future<bool> hasPermissions() async {
+    if (!Platform.isAndroid) return true;
+
+    var locationPermission = await Permission.location.request();
+    if (locationPermission.isDenied) return false;
+    var notificationPermission = await Permission.notification.request();
+    return notificationPermission.isGranted;
+  }
+
   void initializeVolumedeck() async {
+    if (!await hasPermissions()) {
+      print("Failed to get permissions");
+      return;
+    }
+    var androidActivationKey =
+        "on2aKTfiM49rTLylSV04gOpVRVLmZ3RQuJXeQRLCWCmunUbAS1fxcS6PpMNqTJmwgmANPB9Z5bDIIce-evrnTWvksYedUeXqRfjTCgouF08ATaSMlh47i0zOlwuVbESZ2a9UIj0a5jyDpX6ZhKsJGSgaGVPmvBa_Aj2bkGTE4VTf4VQZ49b3FiYcnCeL5kMSDemQxIwCX_a5uvbSF11LQoDEIF-GXAwwxUD9ev7FHUaKUyrZhr2RexPpchJ4G53ivyYITi0YSTmtVb5uN0sAvWwae09USyZZDoIn_B3regLCjgL1g5j_U2jaL5Kvba5QsgY1QqYR40Jt2NesCkP_krbx2kZ3eXe4nmZlpO5jAeGdcsCpd5AAn6q0HIKCVb0fi_ZD5C4FEjfVkwawwa6TH-V5BXddJM6Y8YEIf2mpqeagu-faPsEhunsaHvFF5eIx8JkH4AhzuucQAeVI_zMGiTfD6BBBzksKYfCvx7BlIKnRB6VAxvoXCkloeOh772Gu11D83sXf0jEHhXyd6Pps06Zdf5y5_cJIt46XC4Yl4g4Rt4qIj40pZfK19PeL2QCmhDfI6dSXBlm9czkPnSnzafDS2rOx1x7cz8Ci16KlgASbXoAjW7Xmh61eS5jlVFJoT8QDRdFJZOnNDBM5Noh-jA47t1BUb_Wd1M0NWjCvRzQ";
+    var iOSActivationKey =
+        "bsYZ-aE4j4HPLp4BiqY21s1mUsOvuqHKnaqSb__n9WEWm4QqXDG8sY2dhfds38XAOs_4jj_xFj464f586jy3EnXTc6RUrb_eVscIJGgKNcrGEJQlST5WGUZDbcE6bWKpcrjVIfphaiggvDTKqm9ahcs47E6OfHOeAf2t82rx0mVVqoeMjdEp5UrZk14H64db0ayljEfB_2oR7pRkx0GblIc96QC8KiylDiq0MKiXpDFVrTCchbxSz2Um0aAQPdsOP0un6UPspKoPTFu5CcCzWWe_iLIYxUVqdgpTWhgz2NyExoi7RwQJ70q2HpBMf_Cpy3qdZcdJj604NdzPy6YWiQyCT_rT_ZkbXiKZDBSutCSm9TPha1TKg466RfKLgrhYEDAW5XW4ooJRY9r4PR_-Ly39W-9Qh5TF-Jg6lzMfft5HH6qtp-JnmLIc4xw0HzPMb6QtwcB3F11NBudq5WsLt64kD6JGbnhEi3RE5PYTzGE0nOyQo3vGybas9SQl6XrEHGmgMjaW2om4qLIDsiKc1XYkjFKEFq6dSoYru0TfwhqescYbLW4Z2jN8WtlGpxvqwF8-eNHGVxH0YF2B-GhogTp_cxVx0jSs6TFblgSv7cwZmuMlAvyHx5CLsUuhXM0cE8nSshqxTHb9x1CJS0H5iocpYMefcHHpLN_GWc86N70";
+
     await Volumedeck.initialize(
+      androidActivationKey: androidActivationKey,
+      iOSActivationKey: iOSActivationKey,
       runInBackground: true,
       autoStart: false,
       showStopButtonInAndroidNotification: true,
@@ -44,7 +68,6 @@ class _MyAppState extends State<MyApp> {
         });
       },
     );
-  
   }
 
   @override
@@ -79,8 +102,12 @@ class _MyAppState extends State<MyApp> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   ElevatedButton(
-                    onPressed: () {
-                      Volumedeck.start();
+                    onPressed: () async {
+                      try {
+                        await Volumedeck.start();
+                      } on PlatformException catch (e) {
+                        print("Error: ${e.message}");
+                      }
                     },
                     child: const Text("Start"),
                   ),
